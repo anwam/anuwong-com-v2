e506f851abbc17f78c840a350e976be7cf50bb8c
Anuwong Mongkudkornburee <anuwong.mon@truedigital.com>
2023-09-05 15:15:18 +0700

    feat: add more blogs in home page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "anuwong.com";
-export const SITE_DESCRIPTION = "later is never. now is the time.";
+export const SITE_DESCRIPTION = "later is never, now is the time.";
