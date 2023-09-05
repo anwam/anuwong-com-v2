0d9d9b114306d48a56b9efc099165d4d6e144bb1
Anuwong Mongkudkornburee <anuwong.mon@truedigital.com>
2023-09-05 13:42:17 +0700

    feat: view transition and webring

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "anuwong.com";
-export const SITE_DESCRIPTION = "random things i've learned and want to share!";
+export const SITE_DESCRIPTION = "later is never. now is the time.";
