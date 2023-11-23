8c5f0db45fc34bb774ca2f14b26ca07ec216d45a
anwam <dev.anuwong@gmail.com>
2023-11-23 10:12:45 +0700

    perf: improve image sizing and remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.anuwong.com',
-  integrations: [mdx(), sitemap(), tailwind(), react(), partytown({
-    config: {
-      forward: ['dataLayer.push'],
-    },
-  })]
+  site: "https://www.anuwong.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
