074bbdb2abcf14c5cc12fed8f93136dba155acae
anwam <dev.anuwong@gmail.com>
2024-01-12 19:09:46 +0700

    chore: remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import critters from "astro-critters";
 
 // https://astro.build/config
@@ -19,6 +19,6 @@ export default defineConfig({
       },
     }),
     critters(),
-    compress(),
+    // compress(),
   ],
 });
